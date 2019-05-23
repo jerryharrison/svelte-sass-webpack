12dd3a96fa3be7982db28d8e8d14a27da3e61783
Jerry Harrison <jerry@indie.pw>
2019-05-23 14:45:36 -0400

    Updated to only contain boilerplate items... ooops.

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,12 @@
 <script type="text/javascript">
-    // import Modal from './Modal.svelte';
-    // import Colors from './Colors.svelte';
-    // import Grid from './Grid';
-    import Tabs from './Tabs.svelte';
+    import { Router, Route } from 'svero';
+    import Index from './Routes/Index.svelte';
 </script>
 
 <style>
 
 </style>
 
-
-
-<!--<h3 aria-label="This is not yo mama's components">Swiss Components</h3>-->
-<!--<p>This is not "next-generation." it's a collection of components that are opinionated enough to meet and go above industry standards, while also being "weakly held" enough to be blended and molded into scalable web apps.-->
-<!--</p>-->
-
-<!--<p style="text-align: center;"><a href="http://google.com" aria-label="Google is evil." tooltip-position="top">This is a link to Google</a></p>-->
-<!--<p style="text-align: center;"><a href="http://google.com" aria-label="Google is evil." tooltip-position="right">This is a link to Google</a></p>-->
-<!--<p style="text-align: center;"><a href="http://google.com" aria-label="Google is evil." tooltip-position="bottom">This is a link to Google</a></p>-->
-<!--<p style="text-align: center;"><a href="http://google.com" aria-label="Google is evil." tooltip-position="left">This is a link to Google</a></p>-->
-<!--<p style="text-align: center;"><span class="highlight">This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. </span></p>-->
-<!--<p style="text-align: center;"><span class="underscore">This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. This piece of text should be highlighted. </span></p>-->
-
-
-<!--<Tabs>-->
-<!--    <div slot="tab">Some name</div>-->
-<!--    <div slot="tab">Some other name</div>-->
-<!--    <div slot="panel">panel content 1</div>-->
-<!--    <div slot="panel">panel content 2</div>-->
-<!--</Tabs>-->
+<Router>
+    <Route path="*" component={Index} />
+</Router>
--- src/Colors.svelte
+++
@@ -1,7 +0,0 @@
-<style type="text/sass">
-@import "./Colors.sass"
-</style>
-
-<div class="color_group">
-    <span>TURQUOISE</span>
-</div>
